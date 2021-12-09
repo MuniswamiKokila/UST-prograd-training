596493d638d50055dcaf0f058628de3fc54bc50e
MuniswamiKokila <muniswamikokila3@gmail.com>
2021-12-09 11:57:46 +0530

    added file

--- java-basics/07-12-2021/src/main/java/com/shape/Square.java
+++ java-basics/07-12-2021/src/main/java/com/shape/Square.java
@@ -1,19 +1,8 @@
 package com.shape;
 
-public class Square {
-    double side;
-    Square(double side)
-    {
-        if(side<=0){
-            throw new IllegalArgumentException("Side should not less than or equal to zero");
-        }
-        this.side=side;
-    }
-    public double area(){
+public class Square extends Rectangle {
 
-        return side*side;
-    }
-    public double perimeter(){
-        return 4*side;
+    Square(double side){
+        super(side,side);
     }
 }
---
+++ java-basics/09-12-2021/src/main/java/com/architect/Architect.java
@@ -0,0 +1,39 @@
+package com.architect;
+
+public class Architect {
+
+    double value;
+    public Architect(double value){
+        if(value<=0){
+            throw new IllegalArgumentException("Length Cannot be less than or equal to zero");
+        }
+        this.value=value;
+    }
+    public double centimeterToCentimeter(){
+        return value;
+    }
+    public double centimeterToMeter() {
+
+        return value/100;
+    }
+    public double centimeterToKilometer(){
+        return value/100000;
+    }
+    public double meterToCentimeter(){
+        return value*100;
+    }
+    public double meterToMeter(){
+        return value;
+    }public double meterToKilometer(){
+        return value/1000;
+    }
+    public double kilometerToCentimeter(){
+        return value*100000;
+    }
+    public double kilometerToMeter(){
+        return value*1000;
+    }
+    public double kilometerToKilometer(){
+        return value;
+    }
+}
